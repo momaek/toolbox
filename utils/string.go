73a0552c00496488bb8df20f187d2d6de72eb20a
momaek <momaek17@gmail.com>
2020-11-27 20:27:54 +0800

    add cache and util

---
+++ utils/string.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// BytesToString ..
+func BytesToString(b []byte) string {
+	bytesHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	strHeader := reflect.StringHeader{Data: bytesHeader.Data, Len: bytesHeader.Len}
+	return *(*string)(unsafe.Pointer(&strHeader))
+}
+
+func BytesToString2(b []byte) string {
+	return string(b)
+}
---
+++ utils/string_test.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"testing"
+)
+
+func BenchmarkBytesToString(b *testing.B) {
+	by := []byte("helloworld")
+	for i := 0; i < b.N; i++ {
+		BytesToString(by)
+	}
+
+}
+
+func BenchmarkBytesToString2(b *testing.B) {
+	by := []byte("helloworld")
+	for i := 0; i < b.N; i++ {
+		BytesToString2(by)
+	}
+
+}
