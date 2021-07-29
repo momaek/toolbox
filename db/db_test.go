906d7b749bfbc739cb88f6d99b28527ff744a10b
momaek <momaek17@gmail.com>
2021-07-29 23:24:16 +0800

    update db test

---
+++ db/db_test.go
@@ -0,0 +1,28 @@
+package db
+
+import "testing"
+
+func TestDB(t *testing.T) {
+	conf := Config{
+		Host:     "localhost",
+		Port:     3306,
+		Username: "root",
+		Database: "unit_test",
+	}
+
+	Init(nil, &conf)
+
+	db := Get("THISISDATABASETEST")
+	db.Exec("select * from bill_record")
+
+	r := BillRecord{}
+	db.Find(&r, "id = 1")
+}
+
+type BillRecord struct {
+	ID int64
+}
+
+func (b *BillRecord) TableName() string {
+	return "bill_record"
+}
